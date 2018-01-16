168903dca7d8c4b60d098505b81db18ff8d8606b
wuweiweiwu <wuxx1045@umn.edu>
2018-01-15 20:18:26 -0800

    updating demo

--- examples/basic-example/index.js
+++ examples/basic-example/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader'; // eslint-disable-line import/no-extraneous-dependencies
-import { DragDropContext as dragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+// import { DragDropContext as dragDropContext } from 'react-dnd';
+// import HTML5Backend from 'react-dnd-html5-backend';
+//
+import App from './app';
 
 const rootEl = document.getElementById('app');
-const render = Component => {
-  const wrap = dragDropContext(HTML5Backend);
-  const Wrapped = wrap(Component);
-  ReactDOM.render(
-    <AppContainer>
-      <Wrapped />
-    </AppContainer>,
-    rootEl
-  );
-};
+// const render = Component => {
+//   // const wrap = dragDropContext(HTML5Backend);
+//   const Wrapped = wrap(Component);
+//   ReactDOM.render(
+//     <AppContainer>
+//       <Wrapped />
+//     </AppContainer>,
+//     rootEl
+//   );
+// };
 
 /* eslint-disable global-require, import/newline-after-import */
-render(require('./app').default);
+ReactDOM.render(<App/>, rootEl);
 if (module.hot)
-  module.hot.accept('./app', () => render(require('./app').default));
+  module.hot.accept('./app', () => ReactDOM.render(<App/>, rootEl));
 /* eslint-enable global-require, import/newline-after-import */

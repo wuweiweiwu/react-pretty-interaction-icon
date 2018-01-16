1d00e87b5714ff0ee4d51ee9a4294f1e487681a8
wuweiweiwu <wuxx1045@umn.edu>
2018-01-15 20:44:52 -0800

    basic boiler plate

---
+++ src/react-pretty-interaction-icon.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ReactPrettyInteractionIcon extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default ReactPrettyInteractionIcon;
